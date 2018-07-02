35bb6e7111b267c70682c002f749cfd094369578
Wikonator <topes.jebal@gmail.com>
2018-07-02 16:06:30 +0200

    debugging after merge

--- js/game/Setup.js
+++ js/game/Setup.js
@@ -1,5 +1,8 @@
 function setup(dataFromBack) {
 
+    app.stage.addChild(playArea);
+
+    createPlayerUI();
     enterTownMap();
     resize();
     // enterBuilding();      // <<< Enter player house automatically
